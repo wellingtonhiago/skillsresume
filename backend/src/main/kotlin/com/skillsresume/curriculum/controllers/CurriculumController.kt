fec3270bf993380fd1ca9fedccbda87a26b09547
Jose Carlos <josecarloscjj@gmail.com>
2023-01-11 17:56:05 -0300

    created endpoint getCurriculumById

--- backend/src/main/kotlin/com/skillsresume/curriculum/controllers/CurriculumController.kt
+++ backend/src/main/kotlin/com/skillsresume/curriculum/controllers/CurriculumController.kt
@@ -6,8 +6,10 @@ import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import java.util.*
 
 
 @RestController
@@ -17,6 +19,12 @@ class CurriculumController (var curriculumRepository: CurriculumRepository) {
     @GetMapping
     fun getCurriculums(pageable: Pageable): ResponseEntity<Page<Curriculum>> {
         val curriculum: Page<Curriculum> = curriculumRepository.findAll(pageable)
+        return ResponseEntity.ok(curriculum)
+    }
+
+    @GetMapping(value = ["/{id}"])
+    fun getCurriculumById(@PathVariable id: Long): ResponseEntity<Optional<Curriculum>> {
+        val curriculum: Optional<Curriculum> = curriculumRepository.findById(id)
         return ResponseEntity.ok(curriculum)
     }
 }
