3e0a500e1fc1ff2ec32e4a974ca7616e36026c00
Jose Carlos <josecarloscjj@gmail.com>
2023-01-11 19:23:15 -0300

    fix: getCurriculumSearch

--- backend/src/main/kotlin/com/skillsresume/curriculum/repositories/CurriculumRepository.kt
+++ backend/src/main/kotlin/com/skillsresume/curriculum/repositories/CurriculumRepository.kt
@@ -5,7 +5,6 @@ import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface CurriculumRepository:JpaRepository<Curriculum, Long> {
+interface CurriculumRepository : JpaRepository<Curriculum, Long> {
     fun findByTitleContaining(title: String, pageable: Pageable): Page<Curriculum>
-
 }
