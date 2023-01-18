b486bb1fbf43ba66426e639bfb4becfda9f6a0a2
Jose Carlos <josecarloscjj@gmail.com>
2023-01-18 18:40:10 -0300

    refactor: fix version DTOs

--- backend/src/main/kotlin/com/skillsresume/curriculum/DTOs/v1/SocialNetworkDTO.kt
+++ backend/src/main/kotlin/com/skillsresume/curriculum/DTOs/v1/SocialNetworkDTO.kt
@@ -1,9 +1,9 @@
-package com.skillsresume.curriculum.DTOs
+package com.skillsresume.curriculum.DTOs.v1
 
 import com.skillsresume.curriculum.entities.SocialNetwork
 
 class SocialNetworkDTO() {
-    var idNetwork: Long? = null
+    var idNetwork: Long = 0
     var name: String = ""
     var url: String = ""
 
